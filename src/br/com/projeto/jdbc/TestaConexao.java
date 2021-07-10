38e7527f6703c849e16243c0fb959bf0f7782b04
Lucas Barros Bezerra <lucasbabezerra@gmail.com>
2021-07-10 17:30:48 -0300

    Conectando no banco de dados

---
+++ src/br/com/projeto/jdbc/TestaConexao.java
@@ -0,0 +1,26 @@
+package br.com.projeto.jdbc;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lucas
+ */
+public class TestaConexao {
+    
+    public static void main(String[] args) {
+        
+        try {
+            
+            new ConnectionFactory().getConnection();
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso!");
+            
+        } catch (Exception erro) {
+            
+            JOptionPane.showMessageDialog(null, "Ops aconteceu um erro" + erro);
+            
+        }
+        
+    }
+    
+}
