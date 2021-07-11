946f8cd53515d5e703bc90d1a37b05f1d8de6b56
Lucas Barros Bezerra <lucasbabezerra@gmail.com>
2021-07-11 17:08:04 -0300

    Metodo de cadastrar o cliente

---
+++ src/br/com/projeto/dao/ClientesDAO.java
@@ -0,0 +1,67 @@
+package br.com.projeto.dao;
+
+import br.com.projeto.jdbc.ConnectionFactory;
+import br.com.projeto.model.Clientes;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lucas
+ */
+public class ClientesDAO {
+    
+    private Connection con;
+    
+    public ClientesDAO(){
+        this.con = new ConnectionFactory().getConnection();
+    }
+    
+    public void cadastrarCliente(Clientes obj){
+        
+        try {
+            
+            //Criar Comando sql
+            String sql = "insert into tb_clientes (nome, rg, cpf, email, telefone,cep,endereco,numero,complemento,bairro,cidade,estado)"
+                        + " values (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            
+            //Conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getTelefone());
+            stmt.setString(6, obj.getCelular());
+            stmt.setString(7, obj.getCep());
+            stmt.setString(8, obj.getEndereco());
+            stmt.setInt(9, obj.getNumero());
+            stmt.setString(10, obj.getComplemento());
+            stmt.setString(11, obj.getBairro());
+            stmt.setString(12, obj.getCidade());
+            stmt.setString(13, obj.getUf());
+            
+            //Executar o comando sql
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Cliente cadastrado com Sucesso!");
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar o Cliente" + erro);
+            
+        }
+         
+    }
+    
+    public void alterarCliente(){
+        
+    }
+    
+    public void excluirCliente(){
+        
+    }
+    
+}
