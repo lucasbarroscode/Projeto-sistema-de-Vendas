e24ee101472ab2cb30066c888b42ff5124676f0a
Lucas Barros Bezerra <lucasbabezerra@gmail.com>
2021-07-13 23:40:41 -0300

    Metodos de alterar/excluir clientes

--- src/br/com/projeto/dao/ClientesDAO.java
+++ src/br/com/projeto/dao/ClientesDAO.java
@@ -59,11 +59,66 @@ public class ClientesDAO {
          
     }
     
-    public void alterarCliente(){
+    public void alterarCliente(Clientes obj){
+       
+        try {
+            
+            //Criar Comando sql
+            String sql = "Update tb_clientes set nome=?,rg=?,cpf=?,email=?,telefone=?,celular=?,"
+                    + "cep=?,endereco=?,numero=?,complemento=?,bairro=?,cidade=?,estado=? where id=?";
+                       
+            
+            //Conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getTelefone());
+            stmt.setString(6, obj.getCelular());
+            stmt.setString(7, obj.getCep());
+            stmt.setString(8, obj.getEndereco());
+            stmt.setInt(9, obj.getNumero());
+            stmt.setString(10, obj.getComplemento());
+            stmt.setString(11, obj.getBairro());
+            stmt.setString(12, obj.getCidade());
+            stmt.setString(13, obj.getUf());
+            stmt.setInt(14, obj.getId());
+            
+            //Executar o comando sql
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Cliente alterado com Sucesso!");
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, "Erro ao alterar o Cliente" + erro);
+            
+        }
         
     }
     
-    public void excluirCliente(){
+    public void excluirCliente(Clientes obj){
+      
+        try {
+            
+            //Criar Comando sql
+            String sql = "Delete from tb_clientes where id = ?";
+            
+            //Conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, obj.getId());
+
+            //Executar o comando sql
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null, "Cliente excluido com Sucesso!");
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, "Erro ao excluir o Cliente" + erro);
+            
+        }
         
     }
     public List<Clientes> listarClientes(){
